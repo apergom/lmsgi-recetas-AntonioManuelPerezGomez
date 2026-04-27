edac6e17f4a619f3d65522e1fa038751dbca278d
apergom <aperezg@iesrafaelalberti.es>
2026-04-27 13:04:05 +0200

    realizada funcion para leer el xml como un json

--- js/app.js
+++ js/app.js
@@ -1,12 +1,31 @@
-async function cargarXML() {
+let lista2 = []
 
+async function cargarXML() {
+let
 }
 
 async function leerXML(){
-    let api
+
+    let api = await fetch("./data/recetas.xml")
+    let respuesta = await api.text()
 
     let parser = new DOMParser();
-    let documentoXML = parser.parseFromString()
+    let documentoXML = parser.parseFromString(respuesta, "application/xml")
 
-    let listaXML =
+
+    let arrayJson = []
+
+    for (let i = 0; documentoXML.length > i; i++) {
+        let elemento = documentoXML[i]
+
+        let objeto = {
+            codigo : elemento.getAttribute("codigo"),
+            nombre : elemento.querySelector("nombre"),
+            categoria: elemento.querySelector("categoria"),
+            tiempo : Number(elemento.querySelector("tiempo")),
+            dificultad: Number(elemento.querySelector("dificultad")),
+        }
+        arrayJson.push(objeto)
+    }
+    return arrayJson
 }
