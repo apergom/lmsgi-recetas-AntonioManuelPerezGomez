43eda816ecac477ad9f77b22d724064ec839c810
apergom <aperezg@iesrafaelalberti.es>
2026-04-27 12:48:20 +0200

    Comienzo del examen y estructuracion del proyecto web

---
+++ js/app.js
@@ -0,0 +1,12 @@
+async function cargarXML() {
+
+}
+
+async function leerXML(){
+    let api
+
+    let parser = new DOMParser();
+    let documentoXML = parser.parseFromString()
+
+    let listaXML =
+}
